341dbff20001206395e137035ad6e4338ef11575
Young Yang <galahade@sina.com>
2015-12-17 13:02:35 +0800

    remove evn-single .vagrant files and etl files

--- evn-single/etl/Yema/testsubjobdaemon/testsubjobdaemon_run.bat
+++
@@ -1,3 +0,0 @@
-%~d0
-cd %~dp0
-java -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/log4j-1.2.16.jar;testsubjobdaemon_0_1.jar;test2subjob_0_1.jar;test1subjob_0_1.jar yema.testsubjobdaemon_0_1.testsubjobdaemon --context=Default %* 
--- evn-single/etl/mediation/import-catalogue (2).sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PHP_BIN=`which php`
-
-$PHP_BIN -f /var/www/habitat/public/shell/import.php catalogue
